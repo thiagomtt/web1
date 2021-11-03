f1c83e99972870c5b092b514a4770396bba79c74
Thiago <tmoraes505@gmail.com>
2021-11-03 15:02:03 -0300

    remove

--- SistemaVeiculos/src/db/MySQL/initCliente.sql
+++
@@ -1,7 +0,0 @@
-USE SistemaVeiculos;
-
-INSERT INTO Cliente(cpf, nome, email, senha, sexo, telefone, dataNascimento, papel)
-VALUES ('11111111100', 'Admin', 'admin@gmail.com', 'admin', 'M', '+5516999999999', '2000-05-05', 'ADMIN');
-
-INSERT INTO Cliente(cpf, nome, email, senha, sexo, telefone, dataNascimento, papel)
-VALUES ('22222222200', 'Cliente', 'cliente@gmail.com', 'cliente', 'F', '+5514999999999', '2000-05-05', 'CLIENTE');
